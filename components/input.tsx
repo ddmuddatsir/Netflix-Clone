1517c772131de1a6ae65f1ebb2c6d4d70473ac24
ddmuddatsir <dedemudasir@gmail.com>
2023-10-19 23:42:46 +0700

    Initial commit

---
+++ components/input.tsx
@@ -0,0 +1,68 @@
+import React from "react"
+
+interface inputProps{
+    id: string;
+    onChange: any;
+    value: string;
+    label: string;
+    type?: string;
+}
+
+
+const input: React.FC<inputProps> = ({
+    id,
+    onChange,
+    value,
+    label,
+    type,
+}) => { 
+  return (
+    <div className="relative">
+      <input 
+      onChange={onChange}
+      type={type}
+      value={value}
+      id={id}
+      className="
+        black
+        rounded-md
+        px-6
+        pt-6
+        pb-1
+        w-full
+        text-md
+        text-white
+        bg-neutral-700
+        appearance-none
+        focus:outline-none
+        focus:ring-0
+        peer
+      "
+      placeholder=""/>
+
+      <label 
+      className="
+        absolute
+        text-md
+        text-zinc-400
+        duration-150
+        transform
+        -translate-y-3
+        scalle-75
+        top-4
+        z-10
+        origin-[0]
+        left-6
+        peet-placeholder-show:scale-100
+        peer-placehoder-shown:translate-y-0
+        peer-focus:scale-75
+        peer-focus:-translate-y-3" 
+        
+        htmlFor={id}>
+            {label}
+        </label>
+    </div>
+  )
+}
+
+export default input
